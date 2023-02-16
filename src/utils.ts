85429ef00320a45b0b64673d81ce27abd69fdcfe
Patrissol Kenfack <patrissolkenfack@gmail.com>
2023-02-16 16:25:08 +0100

    Update utils.ts to handle query params in url

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,6 @@
 export function getNotionIdsFromText(text: string): string[] | undefined {
-  const regexNotionUrl =
-    /(?:https?:\/\/)?(?:www\.)?notion\.so\/(?:[0-9a-zA-Z/\-?&=]+)/gm;
-  const regexValidNotionId = /[0-9a-f]{32}/gm;
-
-  const notionUrls = text.match(regexNotionUrl);
-  const notionIds = notionUrls?.map(url => url.substring(url.length - 32));
-  const validNotionIds = notionIds?.filter(notionId =>
-    notionId.match(regexValidNotionId)
-  );
-
-  return validNotionIds;
+    const regexValidNotionId = /[0-9a-f]{32}/gm;
+    const url = new URL(text)
+    const found = url.pathname.match(regexValidNotionId)
+    return found || undefined
 }
