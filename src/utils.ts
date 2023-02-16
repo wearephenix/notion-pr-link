09848516168a3e27c7a409dc4613e1249c8f1025
Patrissol Kenfack <patrissolkenfack@gmail.com>
2023-02-16 16:45:15 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,14 @@
 export function getNotionIdsFromText(text: string): string[] | undefined {
-  const regexNotionUrl =
-    /(?:https?:\/\/)?(?:www\.)?notion\.so\/(?:[0-9a-zA-Z/\-?&=]+)/gm;
-  const regexValidNotionId = /[0-9a-f]{32}/gm;
+    const regexNotionUrl =
+        /(?:https?:\/\/)?(?:www\.)?notion\.so\/(?:[0-9a-zA-Z/\-?&=]+)/gm;
+    const regexValidNotionId = /[0-9a-f]{32}/gm;
 
-  const notionUrls = text.match(regexNotionUrl);
-    
-  const notionIds = notionUrls?.map(url => (new URL(url)).pathname.match(regexValidNotionId));
+    const notionUrls = text.match(regexNotionUrl);
 
-  return notionIds?.map(n => n ? n[0] : undefined).filter(Boolean);
+    const notionIds = notionUrls?.map(url => {
+        const found = new URL(url).pathname.match(regexValidNotionId)
+        return found ? found[0] : ''
+    });
+
+    return notionIds?.filter(s => s !== '');
 }
