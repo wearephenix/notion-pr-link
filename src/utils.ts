ec72a91b24bf1eb05aef64a01fe5d6ae36198eaa
ledouxvi <vincent.l@wearephenix.com>
2025-02-19 14:52:32 +0100

    fix notion url detection

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export function getNotionIdsFromText(text: string): string[] | undefined {
     const regexNotionUrl =
-        /(?:https?:\/\/)?(?:www\.)?notion\.so\/(?:[0-9a-zA-Z/\-?&=]+)/gm;
+        /(?:https?:\/\/)?(?:www\.)?notion\.so\/(?:[0-9a-zA-Z/\-?&=_]+)/gm;
     const regexValidNotionId = /[0-9a-f]{32}/gm;
 
     const notionUrls = text.match(regexNotionUrl) || [];
