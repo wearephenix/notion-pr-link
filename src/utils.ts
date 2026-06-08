ab0c959e5a8e1637e933b829e743fa1c769eabf1
Charlène Servantie <charlene.s@wearephenix.com>
2026-06-08 10:36:07 +0200

    Update notion url regex to fit with app.notion.so

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export function getNotionIdsFromText(text: string): string[] | undefined {
     const regexNotionUrl =
-        /(?:https?:\/\/)?(?:www\.)?notion\.so\/(?:[0-9a-zA-Z/\-?&=_]+)/gm;
+        /(?:https?:\/\/)?(?:app|www)\.notion\.(?:so|com)\/(?:p\/)?(?:[0-9a-zA-Z/\-?&=_]+)/gm;
     const regexValidNotionId = /[0-9a-f]{32}/gm;
 
     const notionUrls = text.match(regexNotionUrl) || [];
